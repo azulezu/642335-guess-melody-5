90a74b57f6535d440a8cf4b98e4ffbde107cfda5
Anna <anna.zueva.mail@inbox.ru>
2020-11-06 13:55:22 +0300

    Добавляет действие для вызова проверки и подсчёта ошибок

--- src/store/action.js
+++ src/store/action.js
@@ -1,3 +1,6 @@
+import {isArtistAnswerCorrect, isGenreAnswerCorrect} from "../game";
+import {GameType} from "../const";
+
 export const ActionType = {
   INCREMENT_MISTAKES: `INCREMENT_MISTAKES`,
   INCREMENT_STEP: `INCREMENT_STEP`,
@@ -9,7 +12,24 @@ export const ActionCreator = {
     type: ActionType.INCREMENT_STEP,
     payload: 1,
   }),
+
   resetGame: () => ({
     type: ActionType.RESET_GAME,
-  })
+  }),
+
+  incrementMistake: (question, userAnswer) => {
+    let answerIsCorrect = false;
+    switch (question.type) {
+      case GameType.ARTIST:
+        answerIsCorrect = isArtistAnswerCorrect(question, userAnswer);
+        break;
+      case GameType.GENRE:
+        answerIsCorrect = isGenreAnswerCorrect(question, userAnswer);
+        break;
+    }
+    return {
+      type: ActionType.INCREMENT_MISTAKES,
+      payload: answerIsCorrect ? 0 : 1,
+    };
+  },
 };
